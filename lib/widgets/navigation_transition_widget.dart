81e07c876ec2043dda4fedbbeae33dde6592362a
farhadprz <f4rh4d.prz@gmail.com>
2024-07-26 11:57:49 +0330

    WIP #1

--- lib/widgets/navigation_transition_widget.dart
+++ lib/widgets/navigation_transition_widget.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
 
 class NavigationTransitionWidget extends StatefulWidget {
-  const NavigationTransitionWidget({super.key});
+  const NavigationTransitionWidget({
+    super.key,
+    required this.scaffoldKey,
+    required this.animationController,
+    required this.railAnimation,
+    required this.navigationRail,
+    required this.navigationBar,
+    required this.appBar,
+    required this.body,
+  });
+
+  final GlobalKey<ScaffoldState> scaffoldKey;
+  final AnimationController animationController;
+  final CurvedAnimation railAnimation;
+  final Widget navigationRail;
+  final Widget navigationBar;
+  final PreferredSizeWidget appBar;
+  final Widget body;
 
   @override
   State<NavigationTransitionWidget> createState() => _NavigationTransitionWidgetState();
 }
 
 class _NavigationTransitionWidgetState extends State<NavigationTransitionWidget> {
+  late final AnimationController controller;
+  late final CurvedAnimation railAnimation;
+  late final ReverseAnimation barAnimation;
+  bool controllerInitialized = false;
+  bool showDivider = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = widget.animationController;
+    railAnimation = widget.railAnimation;
+    barAnimation = ReverseAnimation(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(0.0, 0.5),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      key: widget.scaffoldKey,
+      appBar: widget.appBar,
+      body: widget.body,
+      bottomNavigationBar: ,
+    );
   }
 }
